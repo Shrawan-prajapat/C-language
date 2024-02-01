c7385bdacf7cd237613e00621b7dd08b27143adf
shrawan <shrawan.prajapat.999@gmail.com>
2024-02-01 15:39:12 +0530

    pr 3 add

--- PR-3/progrram 1.c
+++ PR-3/progrram 1.c
@@ -1,3 +1,4 @@
+//Write a Program to print all alphabets from a to z by skipping 3 alphabets using do while loop.
 #include<stdio.h>
 
 main()
@@ -5,8 +6,7 @@ main()
 	char s='a';
 	
 	do{
-		
-		printf("%c",s);
+		printf("%c,\t",s);
 		s+=4;
 	}while(s<='z');
 }
